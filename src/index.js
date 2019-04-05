ba9b6310a108e3384c1fdfec97422107facfdfe9
ronnie14713 <zhang.yanh@husky.neu.edu>
2019-04-05 15:26:09 -0400

    finalproject_1_2_1

--- src/index.js
+++ src/index.js
@@ -5,75 +5,100 @@ function parsetransData(d){
     // geoid: d.geoid,
     // geoid_2: d.geoid_2,
     geography: d.geo_display,
-    total_population: d.HC01_EST_VC01,
-    // drive_alone: d.HC02_EST_VC01,
-    // carpooled: d.HC03_EST_VC01,
-    // public_transportation: d.HC04_EST_VC01
-    age_16_19: d.HC01_EST_VC03,
-    age_20_24: d.HC01_EST_VC04,
-    age_25_44: d.HC01_EST_VC05,
-    age_45_54: d.HC01_EST_VC06,
-    age_55_59: d.HC01_EST_VC07,
-    age_60: d.HC01_EST_VC08
+    total_population: +d.HC01_EST_VC01,
+    // drive_alone: +d.HC02_EST_VC01,
+    // carpooled: +d.HC03_EST_VC01,
+    // public_transportation: +d.HC04_EST_VC01,
+    age_16_19: +d.HC01_EST_VC03,
+    age_20_24: +d.HC01_EST_VC04,
+    age_25_44: +d.HC01_EST_VC05,
+    age_45_54: +d.HC01_EST_VC06,
+    age_55_59: +d.HC01_EST_VC07,
+    age_60: +d.HC01_EST_VC08
+    
   }
 }
 
 const transDataPromise = d3.csv('../data/table_1.csv', parsetransData);
-const geoidPromise = d3.json('../data/geoid_20m.json');
+// const geoidPromise = d3.json('../data/geoid_20m.json');
 
   Promise.all([
-    transDataPromise,
-    geoidPromise,    
+    transDataPromise
+    // geoidPromise,    
     ])
 
 
 
-.then(([transData, geoid]) => {
+.then((transData) => {
     console.log(transData);
-    console.log(geoid);
+    // console.log(geoid);
 
    });
 
 
-//draw a pie chart to show worker's age composition of a certain county
-function pieChart() {
+// const ageComposition = d3.map
+
+
+
+//   }
+
+
+
+//set up the svg's parameters
 const w = 300,
   h = 300,
-  r = 100,
+  r = w/2,
   color = d3.scale.category20c();
 
-const data = d3.
 
 
+//draw a pie chart to show worker's age composition of a certain county
+function pieChart() {
 
-const svg = d3.select('#pie_')
+let svg = d3.select('#pie_chart')
+              .append('svg')
+              .attr('width', w)
+              .attr('height', h)
+              .append('g')
+              .attr('transform', 'translate(' + (w/2) + ',' + (h/2) + ')');
+
+// const arc = d3.svg
+//               .arc()
+//               .outerRadius
 
 
-}
+// }
+
 
 
 //a bar chart to show composition of earnings
 function barChart() {
+  let svg = d3.select('#bar_chart')
+                    .append('svg')
+                    .attr('width', w)
+                    .attr('height', h);
+                    
+                    
 
 }
 
 
-//a tree map to show occupation composition
-function treeMap() {
+// //a tree map to show occupation composition
+// function treeMap() {
 
-}
-
-
-//a bubble chart to show worker's travel time to work 
-function bubbleChart {
-
-}
+// }
 
 
-//a donut chart to show means of transportation
-function donutChart {
-  
-}
+// //a bubble chart to show worker's travel time to work 
+// function bubbleChart {
+
+// }
+
+
+// //a donut chart to show means of transportation
+// function donutChart {
+
+  }
 
 
 
