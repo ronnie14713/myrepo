4b3023c5400e0f5b6c7b44c7e4ed0ae70d131bab
ronnie14713 <zhang.yanh@husky.neu.edu>
2019-04-05 12:53:51 -0400

    finalproject_1_2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,83 @@
-console.log('index.js!');
+// import {select} from 'd3';
+
+function parsetransData(d){
+  return {
+    // geoid: d.geoid,
+    // geoid_2: d.geoid_2,
+    geography: d.geo_display,
+    total_population: d.HC01_EST_VC01,
+    // drive_alone: d.HC02_EST_VC01,
+    // carpooled: d.HC03_EST_VC01,
+    // public_transportation: d.HC04_EST_VC01
+    age_16_19: d.HC01_EST_VC03,
+    age_20_24: d.HC01_EST_VC04,
+    age_25_44: d.HC01_EST_VC05,
+    age_45_54: d.HC01_EST_VC06,
+    age_55_59: d.HC01_EST_VC07,
+    age_60: d.HC01_EST_VC08
+  }
+}
+
+const transDataPromise = d3.csv('../data/table_1.csv', parsetransData);
+const geoidPromise = d3.json('../data/geoid_20m.json');
+
+  Promise.all([
+    transDataPromise,
+    geoidPromise,    
+    ])
+
+
+
+.then(([transData, geoid]) => {
+    console.log(transData);
+    console.log(geoid);
+
+   });
+
+
+//draw a pie chart to show worker's age composition of a certain county
+function pieChart() {
+const w = 300,
+  h = 300,
+  r = 100,
+  color = d3.scale.category20c();
+
+const data = d3.
+
+
+
+const svg = d3.select('#pie_')
+
+
+}
+
+
+//a bar chart to show composition of earnings
+function barChart() {
+
+}
+
+
+//a tree map to show occupation composition
+function treeMap() {
+
+}
+
+
+//a bubble chart to show worker's travel time to work 
+function bubbleChart {
+
+}
+
+
+//a donut chart to show means of transportation
+function donutChart {
+  
+}
+
+
+
+
+
+
+
